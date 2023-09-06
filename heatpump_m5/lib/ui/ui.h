b2b0726e88860dcdd15a2d8f210549526410578a
Calum Cuthill <calum.cuthill@gmail.com>
2023-09-06 21:03:04 +0100

    this works

--- heatpump_m5/lib/ui/ui.h
+++ heatpump_m5/lib/ui/ui.h
@@ -28,10 +28,7 @@ extern "C" {
 void ui_Screen1_screen_init(void);
 extern lv_obj_t *ui_Screen1;
 extern lv_obj_t *ui_Arc_Group;
-extern lv_obj_t *ui_Label2;
 extern lv_obj_t *ui_Arc1;
-extern lv_obj_t *ui_Label_Celsius;
-extern lv_obj_t *ui_Fan_Speed_Control;
 extern lv_obj_t *ui____initial_actions0;
 
 LV_FONT_DECLARE( ui_font_Number);
