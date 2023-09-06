bfc3802de1ef439af1e2f82ac3be9e2348ffa924
Calum Cuthill <calum.cuthill@gmail.com>
2023-09-06 18:57:31 +0100

    does this work with outboard dfu?

--- heatpump_m5/src/m5_core2_hal.cpp
+++ heatpump_m5/src/m5_core2_hal.cpp
@@ -3,18 +3,19 @@
 
 #include <M5Core2.h>
 #include <lvgl.h>
-#include <ArduinoRS485.h>
+// #include <ArduinoRS485.h>
 
 #include "ui_init.h"
-#include "rtc_helpers.h"
+#include "ui.h"
+// #include "rtc_helpers.h"
 
-#define RX_PIN_RS485 13
-#define TX_PIN_RS485 14
-#define DE_PIN_RS485 25
+// #define RX_PIN_RS485 13
+// #define TX_PIN_RS485 14
+// #define DE_PIN_RS485 25
 
 
 
-RS485Class RS485(Serial2, RX_PIN_RS485, TX_PIN_RS485, DE_PIN_RS485, -1);
+// RS485Class RS485(Serial2, RX_PIN_RS485, TX_PIN_RS485, DE_PIN_RS485, -1);
 
 
 void hal_setup() {
@@ -22,10 +23,10 @@ void hal_setup() {
     M5.begin(true, true, true, true, kMBusModeInput);  //Init M5Core2.
 
 
-    Wire1.begin(21, 22, 400000);  //Init I2C_SYS
-    Wire.begin(32, 33, 400000);  //Init I2C_EXT
+    // Wire1.begin(21, 22, 400000);  //Init I2C_SYS
+    // Wire.begin(32, 33, 400000);  //Init I2C_EXT
 
-    setSystemTime(); //from RTC chip
+    // setSystemTime(); //from RTC chip
 
     // pinMode(DE_PIN_RS485, OUTPUT);
     // digitalWrite(DE_PIN_RS485, HIGH);
